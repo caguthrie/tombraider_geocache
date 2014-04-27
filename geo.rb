c184fc7119de0833588a7957538fecebfe842bc3
Christopher Guthrie <christopher.a.guthrie@gmail.com>
2014-04-27 10:14:03 -0400

    first commit

---
+++ geo.rb
@@ -0,0 +1,45 @@
+class String
+  def to_num
+    self.downcase.bytes[0]
+  end  
+end
+
+class Decoder
+
+  attr_accessor :offset_str
+
+  def initialize(str, offset_str)
+    @str = str
+    @offset_str = offset_str
+  end
+
+  def decode
+    i = 0
+    placeholder = ""
+    while i < @str.length
+      ascii_number = @str[i].to_num - offset(i)
+      puts "decoding #{@str[i]} as #{ascii_number}"
+      if ascii_number < 97
+        ascii_number += 26
+      end
+      placeholder << ascii_number.chr
+      i += 1
+    end
+    puts placeholder
+  end
+
+  def offset(num)
+    num = num % @offset_str.length
+    @offset_str[num].to_num - 97  
+  end
+
+end
+
+encrpyted_message = "eceueawimetkvpgztloxqbrrghfhywfyriiev"
+key = "coronadobeach"
+d = Decoder.new(encrpyted_message, key)
+d.decode
+
+
+
+
